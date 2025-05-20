3e10e859eb81e904948b88863c0f8c116b89a744
Amr-222 <144175308+Amr-222@users.noreply.github.com>
2025-05-21 00:55:15 +0300

    Transaction isuues solved

--- DigitalWalletSystem/MainWindow.cpp
+++ DigitalWalletSystem/MainWindow.cpp
@@ -64,12 +64,22 @@ void MainWindow::loadRecentTransactions(int transactionCount) {
         Database db;
         list<Transaction> transactions = db.loadTransactionsFor(currentUser->username);
         if (transactions.empty()) return;
-        int size = transactions.size() -1;
-        for (int i = size; i > size - transactionCount; --i) {
-            if (i < 0) return;
-            const Transaction& t = transactions[i];
+
+
+
+
+
+
+
+
+
+        int count = 0;
+        for (auto it = transactions.rbegin(); it != transactions.rend() && count < transactionCount; ++it, ++count) {
+            const Transaction& t = *it;
+
             QListWidgetItem* item = new QListWidgetItem(ui.listTransactions);
-            QWidget *widget;
+            QWidget* widget;
+
             if (isSentTransation(t)) {
                 widget = createTransactionWidget(
                     "Sent to " + QString::fromStdString(t.receiver),
@@ -86,6 +96,7 @@ void MainWindow::loadRecentTransactions(int transactionCount) {
                     false
                 );
             }
+
             item->setSizeHint(widget->sizeHint());
             ui.listTransactions->setItemWidget(item, widget);
         }
@@ -93,6 +104,15 @@ void MainWindow::loadRecentTransactions(int transactionCount) {
     else {
         qDebug() << "Warning: currentUser is null in loadRecentTransactions()";
     }
+
+
+
+
+
+
+
+
+
 
 }
 
